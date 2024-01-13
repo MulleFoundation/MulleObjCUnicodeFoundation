8e0879f68b6455902db053567f387ce8354bff25
Nat! <nat@mulle-kybernetik.com>
2024-01-13 16:10:35 +0100

    remove dependencies subsumed by mulle-core, fix README.md

--- src/reflect/_MulleObjCUnicodeFoundation-include.h
+++ src/reflect/_MulleObjCUnicodeFoundation-include.h
@@ -14,19 +14,6 @@
 #ifndef _mulle_objc_unicode_foundation_include_h__
 #define _mulle_objc_unicode_foundation_include_h__
 
-// To remove the following dependency (headers and library) completely:
-//   `mulle-sde dependency remove mulle-unicode`
-// (Use 3F86148B-17E3-4BE4-B99C-4F013075A1E7 instead of mulle-unicode, if there are duplicate entries)
-//
-// You can tweak the following #include with these commands:
-//    remove #include: `mulle-sde dependency mark mulle-unicode no-header`
-//    rename              : `mulle-sde dependency|library set mulle-unicode include whatever.h`
-//    reorder             : `mulle-sde dependency move mulle-unicode <up|down>`
-//    toggle #include: `mulle-sde dependency mark mulle-unicode [no-]import`
-//    toggle public       : `mulle-sde dependency mark mulle-unicode [no-]public`
-//    toggle optional     : `mulle-sde dependency mark mulle-unicode [no-]require`
-//    remove for platform : `mulle-sde dependency mark mulle-unicode no-platform-<uname>`
-//        (use `mulle-sourcetree-to-c --unames` to list known values)
-#include <mulle-unicode/mulle-unicode.h>   // mulle-unicode
+/* no headers */
 
 #endif
